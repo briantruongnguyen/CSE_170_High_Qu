9bc3f6e759d890f512cd612d366e5efe1c097fbe
hansyuan <hanzzeh@gmail.com>
2017-02-24 00:28:03 -0800

    profiles update

--- routes/updateProfile.js
+++ routes/updateProfile.js
@@ -2,6 +2,8 @@ var profile = require("../profile.json");
 
 exports.updateProfile = function(req, res) {
 	// Your code goes here
+    
+    /*
 	var profilename=req.query.p_name;
 	var description=req.query.description;
 	var imageURL=req.query.imageURL;
@@ -19,9 +21,22 @@ exports.updateProfile = function(req, res) {
     	break;
   	}
 	}
+    
+    */
+    
+    profile.createdProfiles = []
+    var newProfile={
+		"id" : 1,
+		"name": req.query.p_name,
+		"description": req.query.description,
+		"imageURL":"https://scontent.xx.fbcdn.net/v/t1.0-9/15622540_1892333944331666_4214696052833980642_n.jpg?oh=a891f43b7a5fe18c90ac0d75220bd4b6&oe=593448BF",
+    
+	};
+    
+    profile.createdProfiles[0] = newProfile;
 
 	console.log(profile);
     console.log("This is happening.");
 	//profile.createdProfiles.push(newProfile);
-	res.render('index');
+	res.render('profile', profile);
 }
