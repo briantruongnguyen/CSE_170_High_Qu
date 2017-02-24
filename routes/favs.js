f06fdb5d01d486b87b84f2a5e359014e996b6d51
hansyuan <hanzzeh@gmail.com>
2017-02-24 02:37:14 -0800

    favorites works

--- routes/favs.js
+++ routes/favs.js
@@ -6,3 +6,20 @@ var data = require('../favorites.json');
 exports.view = function(req, res){
   res.render('favs',data);
 };
+
+exports.addFavorite = function(req, res){
+    var t_id = req.params.trail_id;
+    var t_name = req.params.trail_name;
+    var t_desc = req.params.trail_desc;
+    var t_diff = req.params.trail_diff;
+
+    var newjson = {
+        "id"            : t_id,
+        "name"          : t_name,
+        "description"   : t_desc,
+        "difficulty"    : t_diff
+    }
+    console.log("In the javascript file.")
+    console.log(newjson);
+    data.favorites.push(newjson);
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@
  * GET home page.
  */
 var profile = require("../profile.json");
+
+
 exports.view = function(req, res){
 	console.log(profile);
   res.render('index',profile);
 };
+
+
